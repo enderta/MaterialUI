92ac62447bea01a46ad3f4f3f7a60c547084c5b8
enderta <endertanver@gmail.com>
2022-08-15 15:20:01 +0100

    tables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Album from './Components/album/Album'
+import Country from './Components/Tables/Country'
 import './App.css'
 
 const App = () => {
   return (
     <div>
-      <Album />
+    <Country />
     </div>
   )
 }
