c1be4d200bdcd582960818f10475237f28e22509
enderta <endertanver@gmail.com>
2022-08-26 15:55:52 +0100

    fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import React from 'react'
 import FlatCarosel from './Components/Carosel/FlatCarosel'
 import Movie from './Components/movie/Movie'
+import { ImageViewer } from "react-image-viewer-dv"
 const App = () => {
   
 
+  const photos = [
+    {
+        src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        width: 800,
+        height: 600
+    },
+    {
+        src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        width: 1600,
+        height: 900
+    },  {
+      src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      width: 800,
+      height: 600
+  },
+  {
+      src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      width: 1600,
+      height: 900
+  },
+  {
+    src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    width: 800,
+    height: 600
+},
+{
+    src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+    width: 1600,
+    height: 900
+},  {
+  src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  width: 800,
+  height: 600
+},
+{
+  src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  width: 1600,
+  height: 900
+},  {
+  src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  width: 800,
+  height: 600
+},
+{
+  src: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  width: 1600,
+  height: 900
+}
+
+];
   
   return (
     <div>
- demo
+ <ImageViewer>
+            <img src={photos}  alt="Your image" />
+          </ImageViewer>
     </div>
   )
 }
--- src/deneme.js
+++ src/deneme.js
@@ -1,7 +1,6 @@
 import fetch from 'node-fetch';
-import { Headers } from 'node-fetch';
 
-var myHeaders = new Headers();
+let myHeaders = new Headers();
 myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZWFtMThfYWRtaW4iLCJhdXRoIjoiUk9MRV9BRE1JTiIsImV4cCI6MTY2MTYwOTEwMX0.Iwm1vWWehszInPIyIEsCkLRF4Y59ToPZUAv8vCjPGpIXiOQdCIoekckP66W68Y8Nbz_NfYf8qP5pPEY5l4A27A");
 myHeaders.append("Content-Type", "application/json");
 
@@ -14,11 +13,11 @@ var raw = JSON.stringify({
 var requestOptions = {
   method: 'GET',
   headers: myHeaders,
-  
+  body: raw,
   redirect: 'follow'
 };
 
 fetch("https://www.gmibank.com/api/users", requestOptions)
-  .then(response => response.json())
+  .then(response => response.text())
   .then(result => console.log(result))
   .catch(error => console.log('error', error));
