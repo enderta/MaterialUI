5fd64e0de9b35f61cf4f4e5ae49fb7331bb84b55
enderta <endertanver@gmail.com>
2022-08-29 17:52:13 +0100

    express

--- src/Components/spring/Student.jsx
+++ src/Components/spring/Student.jsx
@@ -6,12 +6,20 @@ const Student = () => {
     const[name,setName]=useState('')
     const[address,setAddress]=useState('')
     const[students,setStudents]=useState([])
-     
+    const[search,setSearch]=useState('')
+    const[filter,setFilter]=useState([])
+
+
+   
+    const handleDelete=(id)=>{
+        setStudents(students.filter(student=>student.id!==id))
+    }
+
     const handleClick=(e)=>{
         e.preventDefault()
         const student={name,address}
         console.log(student)
-        fetch("http://localhost:8081/student/add",{
+        fetch("http://localhost:4000/student/add",{
           method:"POST",
           headers:{"Content-Type":"application/json"},
           body:JSON.stringify(student)
@@ -21,56 +29,48 @@ const Student = () => {
       })
     }
     useEffect(()=>{
-        fetch("http://localhost:8081/student/getAll")
+        fetch("http://localhost:4000/student")
         .then(res=>res.json())
         .then((result)=>{
           setStudents(result);
+          setFilter(result)
         }
       )
       },[])
-    
+      const handleSearch=(e)=>{
+        setSearch(e.target.value)
+        const filtered=students.filter(student=>student.name.toLowerCase().includes(e.target.value.toLowerCase()))
+        setFilter(filtered)
+      }
+        
+    console.log(students)
   return (
     <div>
-         <Container>
-        <Paper elevation={3} >
-            <h1 style={{color:"blue"}}><u>Add Student</u></h1>
-
-    <form noValidate autoComplete="off">
-    
-      <TextField id="outlined-basic" label="Student Name" variant="outlined" fullWidth 
-      value={name}
-      onChange={(e)=>setName(e.target.value)}
-      />
-      <TextField id="outlined-basic" label="Student Adress" variant="outlined" fullWidth
-      value={address}
-      onChange={(e)=>setAddress(e.target.value)}
-      />
-      <Button variant="contained" color="secondary" onClick={handleClick}>
-  Submit
-</Button>
-    </form>
-   
-    </Paper>
-    <h1>Students</h1>
-
-    <Paper elevation={3} >
-
-      {students.map(student=>(
-        <Paper elevation={6} style={{margin:"10px",padding:"15px", textAlign:"left"}} key={student.id}>
-         Id:{student.id}<br/>
-         Name:{student.name}<br/>
-         Address:{student.address}
-
+        <Container>
+        <Paper>
+        <form>
+        <TextField label="search" value={search} onChange={handleSearch}/>
+        </form>
         </Paper>
-      ))
-}
+                    <Paper>
+            <form>
+            <TextField label="Name" value={name} onChange={(e)=>setName(e.target.value)}/>
+            <TextField label="Address" value={address} onChange={(e)=>setAddress(e.target.value)}/>
+            <Button onClick={handleClick}>Add</Button>
+            </form>
+            </Paper>
+            <Paper>
+            <ul>
+                {filter.map(student=>(
+                    <li key={student.id}>
+                        {student.name} - {student.address}
+                        <Button onClick={()=>handleDelete(student.id)}>Delete</Button>
+                    </li>
+                ))}
+            </ul>
+            </Paper>
+        </Container>
 
-
-    </Paper>
-
-
-
-    </Container>
     </div>
   )
 }
