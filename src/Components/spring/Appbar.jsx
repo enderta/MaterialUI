8c4a62817a0307a4384b316406b74ea8b25c6083
enderta <endertanver@gmail.com>
2022-08-30 15:02:00 +0100

    express

--- src/Components/spring/Appbar.jsx
+++ src/Components/spring/Appbar.jsx
@@ -1,15 +1,7 @@
 import * as React from 'react';
-import {useState,useEffect} from 'react'
-import {Container,Paper,TextField,Button} from '@mui/material'
+import {Container,AppBar,Box,Toolbar,IconButton,Typography,InputBase} from '@mui/material'
 import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import {SearchIcon,MenuIcon} from '@mui/icons-material/Search';
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
